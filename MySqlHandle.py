1e74270d6f91b093517535101819b9041ece35fc
claudiolammirato <claudio.lammirato@gmail.com>
2021-08-02 11:52:14 +0200

    commit

--- MySqlHandle.py
+++ MySqlHandle.py
@@ -1,14 +1,15 @@
 import mysql.connector as mysql
+import Pass
 
 def connection():
     # enter your server IP address/domain name
     HOST = "remotemysql.com" # or "domain.com"
     # database name, if you want just to connect to MySQL server, leave it empty
-    DATABASE = "kcKWrZgF5e"
+    DATABASE = Pass.database
     # this is the user you create
-    USER = "kcKWrZgF5e"
+    USER = Pass.username
     # user password
-    PASSWORD = "pBn3qUbnL3"
+    PASSWORD = Pass.password
     # connect to MySQL server
     db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
     print("Connected to:", db_connection.get_server_info())
