e305af74e9b0291e82c884a061e81101703af3dd
claudiolammirato <claudio.lammirato@gmail.com>
2021-08-03 13:26:20 +0200

    commit

--- graphic.py
+++ graphic.py
@@ -1,13 +1,19 @@
 import tkinter as tk
+import tkinter as ttk
 
 def grafica():
     window = tk.Tk()
-    window.wm_attributes('-fullscreen','true')
-
-    window.geometry("600x600")
+    #window.wm_attributes('-fullscreen','true')
+    window.geometry("%dx%d+0+0" % (window.winfo_screenwidth(), window.winfo_screenheight()))
+    window.overrideredirect(1)
+    window.resizable(False, False)
     #window.title("Aquarium")
-    closebutton = tk.Button(window, text = 'Click and Quit', command=window.quit)
-    closebutton.grid(row=0, column=0)
+
+    #layout
+    
+
+    closebutton = tk.Button(window, text="My Button2", command=window.quit)
+    closebutton.grid(row=0, column=1)
 
     def quit(self):
         self.root.destroy()
